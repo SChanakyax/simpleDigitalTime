95d1a20ea357d289b98586ce2ade2728b3cc852a
SChanakya <chanakyachana@gmail.com>
2021-05-27 11:38:20 +0530

    comments

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from tkinter.ttk import *
 
 from time import strftime
 
+# root window is created
 root = Tk()
-root.title("Digital Clock")
+root.title("Digital Clock")  # window name
 
 
+# time method
 def time():
-    string = strftime('%H:%M:%S %p')
-    label.config(text=string)
-    label.after(1000, time)
+    timex = strftime('%H:%M:%S %p')
+    label.config(text=timex)
+    label.after(1000, time)  # schedule an action after every second
 
 
 label = Label(root, font=("ds-digital", 80), background="black", foreground="cyan")
@@ -18,4 +20,5 @@ label.pack(anchor='center')
 
 time()
 
-mainloop()
+mainloop()  # listens for events, such as button clicks or key presses, and blocks any code that comes after it from
+# running until the window it's called on is closed
